b03bce70f8b1299ab5bb440f520d70b1d37a72b2
blanc <blancobielsa@gmail.com>
2021-12-12 19:23:07 +0100

    all done

--- Examen Cache/CODIGOS/sesion3-4/3-4print-vm.c
+++ Examen Cache/CODIGOS/sesion3-4/3-4print-vm.c
@@ -4,9 +4,6 @@
 
 int main(void)
 {
-//Este fichero simplemente utiliza la función print_virtual_physical_pte() para imprimir la información
-// de una dirección virtual. El prototipo de esta función está en 3-4print-pte.h y su implementación
-// en 3-4print-pte.c
 	int local = 0xABCDEF01;
 
 	print_virtual_physical_pte(&local, "Local variable\n-----------------");
